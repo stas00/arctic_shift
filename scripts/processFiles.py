3529595a2b5095d020e4c071f8d382c181caa7b0
Stas Bekman <stas@stason.org>
2024-06-06 13:29:23 -0700

    improve the script

--- scripts/processFiles.py
+++ scripts/processFiles.py
@@ -3,12 +3,14 @@ version = sys.version_info
 if version.major < 3 or (version.major == 3 and version.minor < 10):
 	raise RuntimeError("This script requires Python 3.10 or higher")
 import os
+import io
+import json
 from typing import Any, Iterable
 
 from fileStreams import getFileJsonStream
 
 
-fileOrFolderPath = r"<path to file or folder>"
+fileOrFolderPath = sys.argv[-1]
 recursive = False
 
 def processRow(row: dict[str, Any]):
@@ -17,13 +19,21 @@ def processRow(row: dict[str, Any]):
 
 def processFile(path: str):
 	jsonStream = getFileJsonStream(path)
+	path_out = path.replace("zst", "jsonl")
+	assert path != path_out, "broke the assumption of input file's ext to be .zst"
 	if jsonStream is None:
 		print(f"Skipping unknown file {path}")
 		return
-	for i, (lineLength, row) in enumerate(jsonStream):
-		if i % 10_000 == 0:
-			print(f"\rRow {i}", end="")
-		processRow(row)
+	with io.open(path_out, 'w', encoding='utf-8') as f:
+		for i, (lineLength, row) in enumerate(jsonStream):
+			if i % 10_000 == 0:
+				print(f"\rRow {i}", end="")
+			#processRow(row)
+			#row.replace("\n", "\\n")
+			#s = dict(text=row)
+			x = json.dumps(row, sort_keys=True, indent=1, ensure_ascii=False)
+			f.write(x)
+			f.write("\n") # optional
 	print(f"\rRow {i+1}")
 
 def processFolder(path: str):
